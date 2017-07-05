d8d870e6cdcd3494982975fb407d91b7c6aa1111
ELIZABETHNJ <elizabethnj1996@gmail.com>
2017-07-05 10:44:46 +0530

    Create Multiplication.java

---
+++ Multiplication.java
@@ -0,0 +1,21 @@
+# multiply
+import java.io.*;
+import java.util.*;
+public class Multiplication
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner (System.in);
+    int a,n,num;
+    System.out.println("Enter the num");  
+  a=sc.nextInt();
+    System.out.println("Enter the value of n");
+            n=sc.nextInt();
+    for(int i=1;i<=12;i++)
+    {
+       num=a*i;
+         
+         System.out.println(a+"*"+n+"="+num);
+         }
+         }
+         }
